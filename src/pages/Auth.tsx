f28fc4eac32e7f8f6969e389c45562dc48cbe47d
mahdi torkaman <mahditorkaman@pm.me>
2023-05-19 20:00:43 +0430

    fix: some simple things

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
+import  Card  from '../components/Card'
+
 export interface AuthPageProps {}
 
 const Auth: React.FC<AuthPageProps> = ({}) => {
@@ -11,16 +13,16 @@ const Auth: React.FC<AuthPageProps> = ({}) => {
         <div className='absolute bottom-0 right-0 left-0 bg-gradient-to-r from-blue-400 to-emerald-400 h-[50vh] bg-auth'></div>
 
         <div className='h-screen grid justify-center items-center '>
-          <div className='flex flex-col w-[500px] gap-5 justify-center'>
+          <Card shadow='xl' radius='md' className='flex flex-col w-[500px] gap-5 justify-center'>
             <p className='text-center font-bold'>
-              This is the Authentication page
+              LOGIN FORM
             </p>
             <Link
               to='/'
               className='flex gap-2 justify-center items-center py-2 bg-neutral-400 hover:bg-slate-500/70 text-neutral-800 rounded-xl transition'>
               Back to Main Page
             </Link>
-          </div>
+          </Card>
         </div>
       </div>
     </>
