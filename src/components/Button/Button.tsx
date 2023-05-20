cdd0c920c4d388425c3e28d3ee5e0d48d3d4c81d
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-05-20 23:42:40 +0330

    Add: add forgetPassword done  state, Fix: fix Button onClick typescript error

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,9 @@
 import { Button as MantineButton } from '@mantine/core';
-import type { ButtonProps } from '@mantine/core';
+import type { ButtonProps as MantineButtonProps } from '@mantine/core';
+
+interface ButtonProps extends MantineButtonProps {
+  onClick: () => void;
+}
 
 const Button = ({ children, ...otherProps }: ButtonProps) => {
   return <MantineButton {...otherProps}>{children}</MantineButton>;
--- src/pages/Auth/AuthCards/ForgetPasswordCard.tsx
+++ src/pages/Auth/AuthCards/ForgetPasswordCard.tsx
@@ -1,19 +1,30 @@
-import {
-  Button,
-  Card,
-  TextInput,
-  Title,
-} from '../../../components';
+import { useState } from 'react';
+import { Button, Card, TextInput, Title } from '../../../components';
 
 type ForgetPasswordProps = {};
 
 const ForgetPassword = ({}: ForgetPasswordProps) => {
+  const [done, setDone] = useState(false);
   return (
     <Card className='flex flex-col gap-8 p-5 min-w-[460px]'>
-      <Title className="text-center">فراموشی رمز عبور</Title>
-
-      <TextInput label='ایمیل خود را وارد کنید'></TextInput>
-      <Button className='bg-primary h-48 -mt-3'>دریافت ایمیل بازیابی رمزعبور</Button>
+      <Title className='text-center'>فراموشی رمز عبور</Title>
+      {done ? (
+        <>
+          <p className='font-semibold text-center'>
+            لینک بازیابی رمز عبور برای شما ایمیل شد. لطفا ایمیل خود را بررسی
+            کنید.
+          </p>
+        </>
+      ) : (
+        <>
+          <TextInput label='ایمیل خود را وارد کنید'></TextInput>
+          <Button
+            onClick={() => setDone(true)}
+            className='bg-primary h-48 -mt-3'>
+            دریافت ایمیل بازیابی رمزعبور
+          </Button>
+        </>
+      )}
     </Card>
   );
 };
