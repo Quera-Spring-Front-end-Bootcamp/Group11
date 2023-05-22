146e9ffacd1a3c96143df8e1bc969c6f84d1f913
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-05-22 16:04:53 +0430

    fix(Card & Button) : fix Card padding & Button height

--- src/pages/Auth/AuthCards/LoginCard.tsx
+++ src/pages/Auth/AuthCards/LoginCard.tsx
@@ -1,20 +1,34 @@
-import { Card, Title, TextInput, PasswordInput, Button } from '../../../components';
+import {
+  Card,
+  Title,
+  TextInput,
+  PasswordInput,
+  Button,
+} from '../../../components';
 import { Link } from 'react-router-dom';
 
 type LogInCardProps = {};
 
-const LogInCard = ({ }: LogInCardProps) => {
+const LogInCard = ({}: LogInCardProps) => {
   return (
-    <Card className='flex flex-col gap-8 p-5'>
+    <Card className='flex flex-col gap-8'>
       <Title>به کوئرا تسک منیجر خوش برگشتی :)</Title>
 
       <TextInput label='ایمیل'></TextInput>
       <PasswordInput label='رمزعبور'></PasswordInput>
-      <Link className='text-primary text-12 -mt-6' to='/auth/forgetPassword'>رمز عبور را فراموش کرده‌ای؟</Link>
-      <Button>ورود</Button>
+      <Link
+        className='text-primary text-12 -mt-6'
+        to='/auth/forgetPassword'>
+        رمز عبور را فراموش کرده‌ای؟
+      </Link>
+      <Button h={'48px'}>ورود</Button>
       <div className='flex items-center justify-center gap-2'>
         <div>ثبت نام کرده ای؟</div>
-        <Link className='text-primary font-bold' to='/auth/signup'>ثبت نام</Link>
+        <Link
+          className='text-primary font-bold'
+          to='/auth/signup'>
+          ثبت نام
+        </Link>
       </div>
     </Card>
   );
--- src/pages/Auth/AuthCards/SignupCard.tsx
+++ src/pages/Auth/AuthCards/SignupCard.tsx
@@ -9,18 +9,16 @@ import { Checkbox } from '@mantine/core';
 
 type SignUpCardProps = {};
 
-const SignUpCard = ({ }: SignUpCardProps) => {
+const SignUpCard = ({}: SignUpCardProps) => {
   return (
-    <Card className='flex flex-col gap-8 p-5'>
+    <Card className='flex flex-col gap-8'>
       <Title>ثبت‌نام در کوئرا تسک منیجر</Title>
 
       <TextInput label='نام کامل'></TextInput>
       <TextInput label='ایمیل'></TextInput>
       <PasswordInput label='رمزعبور'></PasswordInput>
-      <Checkbox
-      label="قوانین و مقررات را می‌پذیرم."
-      />
-      <Button className='bg-primary h-40'>ثبت نام</Button>
+      <Checkbox label='قوانین و مقررات را می‌پذیرم.' />
+      <Button h={'48px'}>ثبت نام</Button>
     </Card>
   );
 };
