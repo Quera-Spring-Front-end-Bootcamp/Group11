5737cc083e2c9929051f4162ca38b1fa13b820f9
mahdi torkaman <mahditorkaman@pm.me>
2023-05-26 14:54:24 +0430

    Add: Filter and Select-edition

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Paper, Anchor, Select, Text, Button } from '@mantine/core';
 
 interface Filter {
+  uniqueId: number;
   filter1: string;
   filter2: string;
   filter3: string;
@@ -13,20 +14,21 @@ const FilterComponent: React.FC = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
-  const [filters, setFilters] = useState<Filter[]>([{ filter1: '', filter2: '', filter3: '' }]);
+  const [filters, setFilters] = useState<Filter[]>([{ uniqueId: 0, filter1: '', filter2: '', filter3: '' }]);
   const addFilterRow = () => {
-    setFilters([...filters, { filter1: '', filter2: '', filter3: '' }]);
+    const newFilter = { uniqueId: Date.now(), filter1: '', filter2: '', filter3: '' };
+    setFilters([...filters, newFilter]);
   };
 
-  const handleFilterChange = (index: number, field: keyof Filter, value: string) => {
-    const updatedFilters = [...filters];
-    updatedFilters[index] = { ...updatedFilters[index], [field]: value };
+  const handleFilterChange = (uniqueId: number, field: keyof Filter, value: string) => {
+    const updatedFilters = filters.map((filter) =>
+      filter.uniqueId === uniqueId ? { ...filter, [field]: value } : filter
+    );
     setFilters(updatedFilters);
   };
 
-	const removeFilterRow = (index: number) => {
-    const updatedFilters = [...filters];
-    updatedFilters.splice(index, 1);
+  const removeFilterRow = (uniqueId: number) => {
+    const updatedFilters = filters.filter((filter) => filter.uniqueId !== uniqueId);
     setFilters(updatedFilters);
   };
 
@@ -36,62 +38,61 @@ const FilterComponent: React.FC = () => {
 
       {isOpen && (
         <Paper shadow="xs" style={{ marginTop: '10px', padding: '8px' }}>
-					<div className='flex items-center justify-between mb-5'>
-						<Text size="xl" className="font-bold">فیلترها</Text>
-						<Button size="md" variant="link" onClick={toggleDropdown}>X</Button>
-					</div>
+          <div className='flex items-center justify-between mb-5'>
+            <Text size="xl" className="font-bold">فیلترها</Text>
+            <Button size="md" variant="link" onClick={toggleDropdown}>X</Button>
+          </div>
 
-          {filters.map((filter, index) => (
-            <div key={index} className='flex items-center gap-5 mb-5'>
-							<span>تسک هایی که</span>
+          {filters.map((filter) => (
+            <div key={filter.uniqueId} className='flex items-center gap-5 mb-5'>
+              <span>تسک هایی که</span>
               <Select
                 data={[
-									{ value: 'تگ', label: 'تگ' },
-									{ value: 'تاریخ', label: 'تاریخ' },
-									{ value: 'اعضا', label: 'اعضا' },
-									{ value: 'اولویت', label: 'اولویت' },
-								]}
-								searchable
+                  { value: 'تگ', label: 'تگ' },
+                  { value: 'تاریخ', label: 'تاریخ' },
+                  { value: 'اعضا', label: 'اعضا' },
+                  { value: 'اولویت', label: 'اولویت' },
+                ]}
+                searchable
                 value={filter.filter1}
                 placeholder="انتخاب کنید"
-                onChange={(value) => handleFilterChange(index, 'filter1', value)}
+                onChange={(value) => handleFilterChange(filter.uniqueId, 'filter1', value)}
               />
-							<p>آن ها</p>
+              <p>آن ها</p>
               <Select
                 data={[
-									{ value: 'درس', label: 'درس' },
-									{ value: 'کار', label: 'کار' },
-									{ value: 'پروژه', label: 'پروژه' },
-								]}
-								searchable
+                  { value: 'درس', label: 'درس' },
+                  { value: 'کار', label: 'کار' },
+                  { value: 'پروژه', label: 'پروژه' },
+                ]}
+                searchable
                 value={filter.filter2}
                 placeholder="انتخاب کنید"
-                onChange={(value) => handleFilterChange(index, 'filter2', value)}
+                onChange={(value) => handleFilterChange(filter.uniqueId, 'filter2', value)}
               />
               <Select
                 data={[
-									{ value: 'است', label: 'است' },
-									{ value: 'نیست', label: 'نیست' },
-								]}
-								searchable
+                  { value: 'است', label: 'است' },
+                  { value: 'نیست', label: 'نیست' },
+                ]}
+                searchable
                 value={filter.filter3}
                 placeholder="انتخاب کنید"
-                onChange={(value) => handleFilterChange(index, 'filter3', value)}
+                onChange={(value) => handleFilterChange(filter.uniqueId, 'filter3', value)}
               />
 
-							<Button onClick={() => removeFilterRow(index)}>
+              <Button onClick={() => removeFilterRow(filter.uniqueId)}>
                 x
               </Button>
             </div>
           ))}
 
           <Anchor color="blue" onClick={addFilterRow}>
-						افزودن فیلتر جدید
+            افزودن فیلتر جدید
           </Anchor>
         </Paper>
       )}
     </>
   );
 };
-
 export default FilterComponent;
