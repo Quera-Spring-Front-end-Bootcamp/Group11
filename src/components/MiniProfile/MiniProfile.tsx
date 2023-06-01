19e29920d687ed1080202bed12974b82c4e90355
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-06-01 19:29:35 +0430

    fix(MiniProfile): add Link component to MiniProfile

--- src/components/MiniProfile/MiniProfile.tsx
+++ src/components/MiniProfile/MiniProfile.tsx
@@ -1,24 +1,26 @@
+import { Link } from 'react-router-dom';
 import { Avatar } from '../Avatar';
 
 const MiniProfile = () => {
   return (
-    <div
-      onclick={onclick}
-      className='flex justify-center items-center gap-2 w-[fit-content] cursor-pointer'>
-      {/* src.alt,color props & placeholder should be from BackEnd */}
-      <Avatar
-        src={null}
-        alt=''
-        size={'md'}
-        color={'red'}>
-        NM
-      </Avatar>
+    <Link to={'/profile'}>
+      <div
+        className='flex justify-center items-center gap-2 w-[fit-content] cursor-pointer'>
+        {/* src.alt,color props & placeholder should be from BackEnd */}
+        <Avatar
+          src={null}
+          alt=''
+          size={'md'}
+          color={'red'}>
+          NM
+        </Avatar>
 
-      <div className='leading-25 text-[#1E1E1E] text-16 font-medium'>
-        {/* user name should be from BackEnd */}
-        نیلوفر موجودی
+        <div className='leading-25 text-[#1E1E1E] text-16 font-medium'>
+          {/* user name should be from BackEnd */}
+          نیلوفر موجودی
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
