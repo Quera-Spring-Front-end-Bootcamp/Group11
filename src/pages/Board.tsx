1c8c8dedbc81bd7552d57b3f68d55140f085a554
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-06-09 00:29:32 +0430

    fix(Board): fis Board spacing & size

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,19 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
-import { Container } from '../components';
 import { CreateWorkSpaceModal, ShareProjectModal } from '../components/Modal';
 import HeaderBoard from '../components/HeaderBoard';
 import SideBar from '../components/SideBar/SideBar';
+import { BaseSideBar } from '../components/SideBar';
 
 const Board = () => {
   return (
     <>
       <CreateWorkSpaceModal />
       <ShareProjectModal />
-      <Container>
-        <div className='h-screen flex'>
-          <div>{<SideBar />}</div>
-          <div className='w-[calc(100%-340px)] flex flex-col'>
+      <div className='flex flex-row h-screen w-screen'>
+        <div className='bg-[#FAFBFC] h-[100%] w-[23%] order-1'>
+          <BaseSideBar>
+            <SideBar />
+          </BaseSideBar>
+        </div>
+        <div className='bg-[#FAFBFC] h-[100%] w-[77%] order-2'>
+          <div className='pt-[4vh] pr-[16px]'>
             <div className='h-[170px] bg-[#fafbfc] pr-3 pl-6'>
               <HeaderBoard />
             </div>
@@ -23,7 +27,7 @@ const Board = () => {
             </div>
           </div>
         </div>
-      </Container>
+      </div>
     </>
   );
 };
