43f6bbe941d92de5a3b0816182d364e925c4b2c0
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-06-13 16:20:04 +0430

    fix(MiniProfile): connect miniProfile to API

--- src/components/MiniProfile/MiniProfile.tsx
+++ src/components/MiniProfile/MiniProfile.tsx
@@ -1,23 +1,27 @@
 import { Link } from 'react-router-dom';
 import { Avatar } from '../Avatar';
+import { useSelector } from 'react-redux';
 
 const MiniProfile = () => {
+  const { firstname, lastname } = useSelector((state: any) => state.user);
+  const fullName = firstname && lastname && `${firstname} ${lastname}`;
+  const avatarText = firstname && lastname && `${firstname[0]} ${lastname[0]}`;
+
   return (
     <Link to={'/profile'}>
-      <div
-        className='flex justify-center items-center gap-2 w-[fit-content] cursor-pointer'>
+      <div className='flex justify-center items-center gap-2 w-[fit-content] cursor-pointer'>
         {/* src.alt,color props & placeholder should be from BackEnd */}
         <Avatar
           src={null}
           alt=''
           size={'md'}
-          color={'red'} fontSize={'12px'}
+          color={'red'}
+          fontSize={'12px'}
           radius={'50%'}>
-          NM
+          {avatarText}
         </Avatar>
         <div className='leading-25 text-[#1E1E1E] text-16 font-medium'>
-          {/* user name should be from BackEnd */}
-          نیلوفر موجودی
+          {fullName}
         </div>
       </div>
     </Link>
